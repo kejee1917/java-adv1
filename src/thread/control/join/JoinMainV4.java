e470829ec57d6bd95163d31b59c973881def7ff6
kejee1917 <kejee1917@gmail.com>
2026-01-06 18:19:33 +0900

    Thread: sleep, join

---
+++ src/thread/control/join/JoinMainV4.java
@@ -0,0 +1,48 @@
+package thread.control.join;
+
+import util.ThreadUtils;
+
+import static util.MyLogger.log;
+
+public class JoinMainV4 {
+
+    public static void main(String[] args) throws InterruptedException {
+        log("start");
+        SumTask task1 = new SumTask(1, 50);
+        Thread thread1 = new Thread(task1, "thread-1");
+
+        thread1.start();
+
+        // 스레드가 종료될 떄 까지 대기
+        log("join() 대기 시작");
+        thread1.join(1000);
+        log("join() 대기 완료");
+
+        log("task1.result = " + task1.result);
+        log("end");
+    }
+
+    static class SumTask implements Runnable {
+
+        int startValue;
+        int endValue;
+        int result;
+
+        public SumTask(int startValue, int endValue) {
+            this.startValue = startValue;
+            this.endValue = endValue;
+        }
+
+        @Override
+        public void run() {
+            log("작업 시작");
+            ThreadUtils.sleep(2000);
+            int sum = 0;
+            for (int i = startValue; i <= endValue; i++) {
+                sum += i;
+            }
+            result = sum;
+            log("작업 완료 result = " + result);
+        }
+    }
+}
